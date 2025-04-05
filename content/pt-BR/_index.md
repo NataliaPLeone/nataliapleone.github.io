91e114879ce145b26b0f34a50f634c52e699bcd8
Natalia PL <np2020220@gmail.com>
2025-04-05 00:08:07 -0300

    commit portugues

---
+++ content/pt-BR/_index.md
@@ -0,0 +1,5 @@
+---
+title: "Página Inicial"
+date: 2025-04-04
+---
+Olá! Esse é o conteúdo em português!
---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeployment: Building site...\033[0m"
+hugo
+
+echo -e "\033[0;32mDeployment: Committing changes...\033[0m"
+cd public
+git init
+git add .
+git commit -m "Deploy to GitHub Pages"
+
+echo -e "\033[0;32mDeployment: Pushing to GitHub...\033[0m"
+git remote add origin https://github.com/NataliaPLeone/NataliaPLeone.github.io
+git push -f origin master
